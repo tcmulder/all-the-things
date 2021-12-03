f8cc7c24b778c61051d019e613e15117b842c3f9
Tomas Mulder <tcmulder@gmail.com>
2021-12-02 18:42:20 -0700

    Use safer loop limit

--- all-the-things/templates/archive.php
+++ all-the-things/templates/archive.php
@@ -5,7 +5,7 @@
     <?php wp_head(); //mandatory ?>
 </head>
 <body>
-    <?php query_posts('posts_per_page=-1&post_type=all-the-things&orderby=title&order=ASC'); ?>
+    <?php query_posts('posts_per_page=999&post_type=all-the-things&orderby=title&order=ASC'); ?>
     <?php if(have_posts()) : ?>
         <ul class="things">
             <?php while(have_posts()) : the_post(); ?>
